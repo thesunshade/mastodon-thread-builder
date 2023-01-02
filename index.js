0d2193b2d49a2b28661ffa9a4b437c2d66c469bb
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2023-01-02 10:35:37 +0530

    upate

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const maximumCharacters = 500;
+let maximumCharacters = 500;
 // const textAreaElement = document.querySelector("#message");
 const textAreas = document.querySelectorAll(".textarea");
 const typedCharactersElements = document.querySelectorAll(".typed-characters");
 // console.log(typedCharactersElements);
 const copyButtons = document.querySelectorAll(".copy");
+const trashButtons = document.querySelectorAll(".trash-button");
+
 const totalNumberOfPostsElement = document.querySelector("#totalNumberOfPosts");
 const copyAllButton = document.querySelector("#copyAll");
+let numberOfPosts = 0;
+let addCountAtEndOfPost = true;
 
-// totalNumberOfPosts.innerHTML = 0;
+if (addCountAtEndOfPost) {
+  maximumCharacters = maximumCharacters - 6;
+}
 
 // initialize the page
 for (let x = 0; x < textAreas.length; x++) {
@@ -15,23 +21,23 @@ for (let x = 0; x < textAreas.length; x++) {
 }
 
 function countCharacters(postNumber) {
-  // console.log(postNumber);
   let typedCharacters = 0;
   const thisTextArea = textAreas[postNumber];
   const thisTypedCharactersElement = typedCharactersElements[postNumber];
-  // console.log(thisTypedCharactersElement);
 
   const foundLinks = thisTextArea.value.match(/http.+?(?!\S)/g);
-  const messageNoLinks = thisTextArea.value.replace(/http.+?(?!\S)/g, "");
+  let messageNoLinks = thisTextArea.value.replace(/http.+?(?!\S)/g, "");
+  messageNoLinks = messageNoLinks.replace(/(@.+?)(@.+?)(?!\S)/g, "$1");
+
   let linkCharacterTotal = 0;
   if (foundLinks) {
     linkCharacterTotal = 23 * foundLinks.length;
   }
+
   typedCharacters = messageNoLinks.length + linkCharacterTotal;
 
   /*Display the number of characters typed.   */
   thisTypedCharactersElement.textContent = maximumCharacters - typedCharacters;
-  // console.log(maximumCharacters - typedCharacters);
 
   if (typedCharacters > maximumCharacters) {
     thisTypedCharactersElement.classList.add("text-danger");
@@ -49,36 +55,41 @@ function countCharacters(postNumber) {
 }
 
 function checkNumberOfPosts() {
-  let numberOfPosts = 0;
+  numberOfPosts = 0;
   for (let x = 0; x < textAreas.length; x++) {
     if (textAreas[x].value.length > 0) {
-      numberOfPosts++;
+      numberOfPosts = x + 1;
       // console.log(numberOfPosts)
     }
     totalNumberOfPostsElement.textContent = numberOfPosts;
-    console.log(totalNumberOfPostsElement);
   }
 }
 
+//initialize listeners
 for (let x = 0; x < textAreas.length; x++) {
   textAreas[x].addEventListener("input", event => {
     countCharacters(event.target.attributes.postnumber.value);
-    // console.log(event.target.attributes.postnumber.value);
   });
   copyButtons[x].addEventListener("click", event => {
-    navigator.clipboard.writeText(textAreas[x].value);
+    let postContent = `\n(${x}/${numberOfPosts})`;
+    navigator.clipboard.writeText(textAreas[x].value + postContent);
+  });
+  trashButtons[x].addEventListener("click", event => {
+    console.log(textAreas[x].textContent);
+    textAreas[x].value = "";
+    event.preventDefault();
+    checkNumberOfPosts();
+    countCharacters(x);
   });
 }
 
 copyAllButton.addEventListener("click", event => {
   let allAreas = "";
   for (let x = 0; x < textAreas.length; x++) {
-    // console.log(textAreas[x].value.length);
-
     if (textAreas[x].value.length > 0) {
       allAreas += textAreas[x].value;
       if (x < textAreas.length - 1) {
-        allAreas += `\n\n----------------------------------\n\n`;
+        allAreas += `\n\n(${x + 1}/${numberOfPosts})------------------------\n\n`;
       }
     }
   }
